8dc23b5f84f438be6a977a1553ce7c4f69607d67
edumes <66120423+EdUuUms@users.noreply.github.com>
2021-09-29 21:39:32 -0300

    Add files via upload

---
+++ internet_global_downloader/main.py
@@ -0,0 +1,32 @@
+from tkinter import Button, font
+from tkinter.constants import NONE, TRUE
+from tkinter.font import Font
+import PySimpleGUI as sg
+from PySimpleGUI.PySimpleGUI import ColorChooserButton, Input, Window, theme_text_color
+from pytube import YouTube
+from time import sleep, time
+
+def IGD():  #tela inicial
+    sg.theme('Black')
+    layout = [
+        [sg.Text('INTERNET GLOBAL DOWNLOADER', font='Courier 16')],
+        [sg.InputText(text_color='orange')],
+        [sg.Button('Download')]
+    ]
+    return sg.Window('IGD', layout=layout, finalize=TRUE, element_justification='c')
+
+def download_video_yt_720p():   #baixar video em 720p
+    video = YouTube(link_yt)
+    video.streams.get_by_itag(22).download()
+
+janela1, janela2 = IGD(), None
+
+while True:
+    window, event, values = sg.read_all_windows()
+    link_yt = (values[0])
+    if window == janela1 and event == sg.WIN_CLOSED:
+        break
+    else:
+        download_video_yt_720p()
+        sg.popup('Download Concluido!')
+window.close()
